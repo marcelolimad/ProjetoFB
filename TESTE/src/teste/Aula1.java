1ed1d8f3b2f84e7a352cf9f5ca6710a82741a946
marcelolimad <marcelolimad@gmail.com>
2022-10-05 14:02:08 -0300

    Commit data 05-10-2022

--- TESTE/src/teste/Aula1.java
+++ TESTE/src/teste/Aula1.java
@@ -13,6 +13,7 @@ public class Aula1 {
 		double nota2;
 		double nota3;
 		double media;
+		double resto;
 
 		System.out.printf("Informe nota 1: ");
 		nota1 = N.nextDouble();
@@ -22,8 +23,11 @@ public class Aula1 {
 		nota3 = N.nextDouble();
 
 		media = (nota1 + nota2 + nota3) / 3;
+		
+		resto = nota1 % nota2;
 
 		System.out.println(media);
+		System.out.println(resto);
 
 		if (media >= 50) {
 			if (media >= 60) {
@@ -39,6 +43,69 @@ public class Aula1 {
 	 else if(media>=40) {System.out.println("Reprovado");}
 	 
 	 else{System.out.println("Saiu");}
+		
+	    /*String md =  Double.toString(media);*/
+		
+		int i = (int)media;
+		
+		switch (i) {
+		case 50: System.out.println("aprovado-SW");
+			
+			break; 
+
+		default: System.out.println("Valeu-SW");
+			break;
+		}
+		
+		int numero = 0;
+		
+		while (numero <= 5) { /* Verificar e depois executa */
+		
+			System.out.println("o numero é " + numero);
+			numero ++;
+		}
+		
+		int numero2 = 0;
+		
+		do { /* primeiro executa e depois verificar */
+		
+			System.out.println("O numero2 atual é: " + numero2);
+			numero2 ++;
+			
+		}while (numero2<= 10);
+		
+		/* estrutura de repitição com break para o codigo */
+		
+		for (int num = 0; num <=10; num ++  ) {
+			
+			if (num == 5) {
+			
+			System.out.println("Encontrei o numero 5");
+			
+			break; 
+			}
+		}
+			
+			/* estrutura de repitição com "continue" para continuar o codigo */
+			
+			for (int nume = 0; nume <=10; nume ++  ) {
+				
+				if (nume == 2 || nume == 1 || nume == 9) {
+				
+				System.out.println("Encontrei o nume " + nume);
+				 
+				continue;
+				}
+				
+				System.out.println("processando");
+			}
+			
+			
+			
+		}
+		
+		
+		
 
 	/* Operadores ternários são para micro validações */
 
@@ -50,4 +117,4 @@ public class Aula1 {
 	
 	
 }
-}
+
