7a8238d2a95f0fc292f251c32e31ba91a602fcd9
marcelolimad <marcelolimad@gmail.com>
2023-03-07 15:58:58 -0300

    Trabalho 07-03

--- Novo/src/Arquivos/ObjJSON.java
+++ Novo/src/Arquivos/ObjJSON.java
@@ -5,12 +5,32 @@ import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import Arquivos.*;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
+import CursoThread.ImplemetacaoFilaThread;
+import CursoThread.ObjFilaThread;
 
 public class ObjJSON extends JDialog {
 
@@ -22,21 +42,36 @@ public class ObjJSON extends JDialog {
 		private JPanel  jPanel = new JPanel(new GridBagLayout());
 		
 		private  JLabel descricaoNome = new JLabel( "Nome" );
-		private  JTextField compoTextoNome = new JTextField();
+		private  JTextField campoTextoNome = new JTextField();
+		
+		private  JLabel descricaoLogin = new JLabel( "Login" );
+		private  JTextField campoTextoLogin = new JTextField();
 		
 		private  JLabel descricaoCPF = new JLabel( "CPF" );
 		private  JTextField campoTextoCPF = new JTextField();
-		private  JButton jButton = new JButton("Add Lista"); 
+		
+		private  JLabel descricaoSenha = new JLabel( "Senha" );
+		private  JTextField campoTextoSenha = new JTextField();
+		
+		
+		
+		private  JButton jButton = new JButton("Cadastrar"); 
 		private  JButton jButton2 = new JButton("Parar"); 
 		private  JButton jButton3 = new JButton("Iniciar"); 
-		private  JButton jButton4 = new JButton("Uplado"); 
+		private  JButton jButton4 = new JButton("Upload Documentos"); 
+		
+		
+		private Thread JsonThread1;
+		
+		
+		private JsonThread jsonT = new JsonThread();
 		
 		
 		
-public ObjJSON () {
+public ObjJSON ()   {
 			
 			setTitle("Minha Tela");
-			setSize(new Dimension(440, 340));
+			setSize(new Dimension(640, 640));
 			setLocationRelativeTo(null);
 			setResizable(false);
 			//=================================
@@ -53,11 +88,21 @@ public ObjJSON () {
 			jPanel.add(descricaoNome, gridBagConstrainsts);
 			
 			
-			compoTextoNome.setPreferredSize(new Dimension(300,25));
+			campoTextoNome.setPreferredSize(new Dimension(300,25));
 			gridBagConstrainsts.gridy ++;
 			//mostrarTempo.setEditable(false);
-			jPanel.add(compoTextoNome, gridBagConstrainsts);
+			jPanel.add(campoTextoNome, gridBagConstrainsts);
 			
+			
+			descricaoLogin.setPreferredSize(new Dimension(300, 25));
+			gridBagConstrainsts.gridy ++;
+			jPanel.add(descricaoLogin, gridBagConstrainsts);
+			
+			
+			campoTextoLogin.setPreferredSize(new Dimension(300,25));
+			gridBagConstrainsts.gridy ++;
+			//mostrarTempo.setEditable(false);
+			jPanel.add(campoTextoLogin, gridBagConstrainsts);
 			
 			//====================
 			
@@ -72,12 +117,26 @@ public ObjJSON () {
 			//mostrarTempo2.setEditable(false);
 			jPanel.add(campoTextoCPF, gridBagConstrainsts);
 			
+			descricaoSenha.setPreferredSize(new Dimension(300, 25));
+			gridBagConstrainsts.gridy ++;
+			jPanel.add(descricaoSenha, gridBagConstrainsts);
+			
+			
+			campoTextoSenha.setPreferredSize(new Dimension(300,25));
+			gridBagConstrainsts.gridy ++;
+			//mostrarTempo2.setEditable(false);
+			jPanel.add(campoTextoSenha, gridBagConstrainsts);
+			
 			gridBagConstrainsts.gridwidth =1;
 			
 			jButton.setPreferredSize(new Dimension (92, 25));
 			gridBagConstrainsts.gridy ++;
 			jPanel.add(jButton, gridBagConstrainsts);
 			
+			jButton4.setPreferredSize(new Dimension (300, 25));
+			gridBagConstrainsts.gridy ++;
+			jButton4.setEnabled(false);
+			jPanel.add(jButton4, gridBagConstrainsts);
 			
 			jButton2.setPreferredSize(new Dimension (92, 25));
 			gridBagConstrainsts.gridx ++;
@@ -87,16 +146,128 @@ public ObjJSON () {
 			gridBagConstrainsts.gridx ++;
 			jPanel.add(jButton3, gridBagConstrainsts);
 			
-			jButton4.setPreferredSize(new Dimension (300, 25));
-			gridBagConstrainsts.gridy = -1;
-			//jButton4.setEnabled(false);
-			jPanel.add(jButton4, gridBagConstrainsts);
+			
 			
 			//jButton4.setPreferredSize(new Dimension (300, 25));
 			//gridBagConstrainsts.gridy ++;
 			//jButton4.setEnabled(false);
 			//jPanel.add(jButton4, gridBagConstrainsts);
 		
+			
+			List<User> usuario = new ArrayList<User>();
+			
+	jButton.addActionListener(new ActionListener()  {
+				
+				@Override
+				public void actionPerformed(ActionEvent e) {
+								
+					
+					
+						Thread JsonThread1 = new Thread();
+						
+						JsonThread1.start();
+						
+					
+					
+					
+					User usuario1 = new User();
+					usuario1.setNome(campoTextoNome.getText());
+					usuario1.setCpf(campoTextoCPF.getText());
+					usuario1.setLogin(campoTextoLogin.getText());
+					usuario1.setSenha(campoTextoSenha.getText());
+					
+					
+					usuario.add(usuario1);
+				
+				
+				
+
+					System.out.println(usuario);
+					
+				
+					Gson gson = new GsonBuilder().setPrettyPrinting().create();
+					
+					
+					
+					String listasUser = gson.toJson(usuario);
+					
+					//String listasUser = new Gson().toJson(usuario);
+					
+					System.out.println(listasUser);
+					
+					
+					
+					
+					
+					FileWriter arquivo = null;
+					try {
+						arquivo = new FileWriter("D:\\Users\\marcelomls\\git\\repository\\Novo\\src\\Arquivos\\Arq.json");
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					
+					try {
+						arquivo.write(listasUser);
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					try {
+						arquivo.flush();
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					try {
+						arquivo.close();
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					
+				
+				  
+			
+					
+				
+					//if(fila == null) {
+					//	fila = new ImplemetacaoFilaThread();
+					//	fila.start();
+					//}
+					
+					//for (int q = 0; q < 30; q++) {
+					
+					//ObjFilaThread filaThread = new ObjFilaThread();
+					//filaThread.setNome(mostrarTempo.getText());
+					//filaThread.setEmail(mostrarTempo2.getText()+" "+ q);
+					
+					//fila.add(filaThread);
+					//}
+					//jButton.setEnabled(false);
+					//jButton2.setEnabled(true);
+				
+				}
+	
+	});
+	jButton2.addActionListener(new ActionListener() {
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			//Thread thtime1 = new Thread();
+			//thtime1.stop();
+			
+			//jButton2.setEnabled(false);
+			//jButton.setEnabled(true);
+		
+			
+			Thread JsonThread1 = new Thread();
+			
+			JsonThread1.stop();
+			
+		}
+	} );
+			
            
 			
 			add(jPanel, BorderLayout.WEST);
@@ -107,7 +278,9 @@ public ObjJSON () {
 			
 			
 			setVisible(true);
-}
+
 	}
+
+}
 
 
